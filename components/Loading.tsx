a27f4f6d304dd3e3b69c1b192d13b59a806b2806
morshedulmunna <morshedulmunna1@gmail.com>
2023-07-20 17:27:23 +0600

    top layout fix

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,17 +1,11 @@
-import { FC } from 'react';
-import { Dots } from 'react-activity';
-import 'react-activity/dist/library.css';
-type Props = {
-  loading?: boolean;
-};
+import { Dots } from "react-activity";
+import "react-activity/dist/library.css";
 
-const Loading: FC<Props> = ({ loading = true }) => {
-  if (!loading) return null;
+const Loading = () => {
   return (
     <>
-      <div className="h-screen w-full"></div>
-      <div className="fixed left-0 top-0 z-[100] grid h-screen w-screen  place-content-center place-items-center bg-slate-900/40 backdrop-blur-[2.2px]">
-        <Dots size={25} className="text-white" />
+      <div className="flex justify-center  items-center h-full w-full ">
+        <Dots size={25} className="text-black" />
       </div>
     </>
   );
